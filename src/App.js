83e64c2111726e12524b6a5435f3ebd516213f22
Andre Almeida <andre-silva78@hotmail.com>
2022-06-02 19:00:31 -0300

    First version of our income tracker

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
+import { useState, useEffect } from 'react'
+import Header from './components/Header/Header';
+import IncomeForm from './components/IncomeForm/IncomeForm';
+import IncomeList from './components/IncomeList/IncomeList';
 
 function App() {
+  const [income, setIncome] = useState([])
+  const [totalIncome, setTotalIncome] = useState(0)
+
+  useEffect(() => {
+    let tempIncome = 0;
+    income.forEach(income => {
+      tempIncome += parseInt(income.price)
+    })
+    setTotalIncome(tempIncome)
+  }, [income])
+
   return (
     <div className="App">
-     
+     <Header totalIncome={totalIncome} />
+     <IncomeForm income={income} setIncome={setIncome} />
+     <IncomeList income={income} setIncome={setIncome} />
     </div>
   );
 }
